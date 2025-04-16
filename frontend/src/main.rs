adef894e4cab3c03c34f5ea1616219dde780b041
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-04-16 09:54:57 +0200

    Addded some new pages; wrote basic router in main.rs; started writing home.rs

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,115 @@
-fn main() {
-    println!("Hello, MathNet!");
+/*
+This module includes router (currently 
+the project is relatively small.)
+*/
+
+use crate::pages::home;
+use crate::pages::login;
+
+
+use sauron::prelude::*;
+use wasm_bindgen::prelude::*;
+use web_sys::window::*;
+
+#[derive(Debug, Clone)]
+enum Msg {
+    NavigateTo(String),
+    UlrChanged,
+
 }
+
+#[derive(Debug,Clone,PartialEq)]
+enum Page {
+Home,
+Login,
+UserProfile,
+Settings,
+Register,
+Chat,
+Docs,
+ListOfExercises,
+Exercise
+}
+
+struct App {
+    page: Page,
+}
+
+
+
+impl App{
+    fn parse_location() -> Page {
+        let path = window().unwrap().location().pathname().unwrap_or_default();
+        match path.as_str() {
+            "/" => Page::Home,
+            "/login" => Page::Login,
+            "/user_profile" => Page::UserProfile,
+            "/settings" => Page::Settings,
+            "/register" => Page::Register,
+            "/chat" => Page::Chat,
+            "/docs" => Page:Docs,
+            "/list_of_exercises" => Page::ListOfExercises,
+            "/exercise" => Page::Exercise,
+        }
+    }
+}
+    impl Application<Msg> for App {
+        fn update(&mut self, msg: Msg) -> Cmd<Self, Msg> {
+            match msg {
+                Msg::NavigateTo(path) => {
+                    let _ = window().unwrap().history().unwrap().push_state_with_url(&wasm_bindgen::JsValue::NULL, "", Some(&path));
+                    self.page = Self::parse_location();
+                }
+                Msg::UrlChanged => {
+                    self.page = Self::parse_location();
+                }
+            }
+            Cmd::none()
+        }
+    
+        fn view(&self) -> Node<Msg> {
+            let nav = nav(
+                [],
+                [
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/".to_string()))], [text("Home")]),
+                    text(" | "),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/login".to_string()))], [text("Login")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/user_profile".to_string()))], [text("User profile")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/register".to_string()))], [text("User profile")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/settings".to_string()))], [text("Settings")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/chat".to_string()))], [text("Chat")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/docs".to_string()))], [text("Documentation")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/list_of_exercises".to_string()))], [text("List of exercises")]),
+                    a([href("#"), on_click(|_| Msg::NavigateTo("/exercise".to_string()))], [text("Exercise")]),
+                ],
+            );
+    
+            let content = match self.page {
+                Page::Home => home::view(),
+                Page::Login => login::view(),
+                Page::UserProfile => user_profile::view(),
+                Page::Settings => settings::view(),
+                Page::Register => register::view(),
+                Page::Chat => chat::view(),
+                Page::Docs => docs::view(),
+                Page::ListOfExercise => list_of_exercises::view(),
+                Page::Exercise => exercise::view(),
+            };
+    
+            div([], [nav, hr([]), content])
+        }
+    
+        fn init(&mut self) -> Cmd<Self, Msg> {
+            self.page = Self::parse_location();
+            sauron::dom::add_event_listener_with_event("popstate", |_| Msg::UrlChanged);
+            Cmd::none()
+        }
+    }
+    
+    #[wasm_bindgen(start)]
+    pub fn main() {
+        console_error_panic_hook::set_once();
+        Program::mount_to_body(App {
+            page: Page::Home,
+        });
+    }
---
+++ frontend/src/pages/home.rs
@@ -0,0 +1,40 @@
+use sauron::prelude::*,
+
+pub fn view() -> Node<crate::Msg> {
+    html(
+        [],
+        [
+            head(
+                [],
+                [
+                    title([], [text("Home Page")]),
+                    meta([name("description"), content("Example of common HTML tags")], []),
+                    link([rel("stylesheet"), href("styles.css")], []),
+                ],
+            ),
+            body(
+                [],
+                [
+                    header(
+                        [],
+                        [
+                            h1([], [text("Welcome to the Home Page!")]),
+                            nav(
+                                [],
+                                [
+                                    a([href("/")], [text("Home")]),
+                                    text(" | "),
+                                    a([href("/login")], [text("Login")]),
+                                    a([href("/register")], [text("Register")]),
+                                    a([href("/docs")],[text("Documentation")]),
+                                    // a([href("https://github.com/iruspro/mathnet.git")],[text(GitHub repository)] - nekaj ne dela - kako linkati zunanjo stran
+
+                                ],
+                            ),
+                        ],
+                    ),
+                        ]
+                    )
+                ]
+            )
+}
