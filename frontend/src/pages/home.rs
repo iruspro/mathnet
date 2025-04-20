c50a5469afef816b195f3e443d94c960a733e715
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-04-20 21:20:34 +0200

    Made testing webpage and it works!

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,42 +1,11 @@
 use sauron::prelude::*;
 use crate::messages::Msg;
-use sauron::html::{meta,link,title};
 
 pub fn view() -> Node<Msg> {
-    html(
-        [],
-        [
-            head(
-                [],
-                [
-                    title([],[text("Home Page")]),
-                    meta([name("description"), content("Example of common HTML tags")], []),
-                    link([rel("stylesheet"), href("styles.css")], []),
-                ],
-            ),
-            body(
-                [],
-                [
-                    header(
-                        [],
-                        [
-                            h1([], [text("Welcome to the Home Page!")]),
-                            nav(
-                                [],
-                                [
-                                    a([href("/")], [text("Home")]),
-                                    text(" | "),
-                                    a([href("/login")], [text("Login")]),
-                                    a([href("/register")], [text("Register")]),
-                                    a([href("/docs")],[text("Documentation")]),
-                                    // a([href("https://github.com/iruspro/mathnet.git")],[text(GitHub repository)] - nekaj ne dela - kako linkati zunanjo stran
-
-                                ],
-                            ),
-                        ],
-                    ),
-                        ]
-                    )
-                ]
-            )
+    node!{
+        <main>
+        <h1> "Welcome to mathnet!" </h1>
+        <input type="button" on_click=|_| {Msg::GoToDocsPage} value="Go to docs"> </input>
+        </main>
+    }
 }
