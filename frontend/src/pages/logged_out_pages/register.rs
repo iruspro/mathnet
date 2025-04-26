cb7d10adf1d95c72c9e167a73e7579e251f217d1
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-04-26 13:03:49 +0200

    Made better layout for login page and created register page and basic underlying logic for both.

--- frontend/src/pages/logged_out_pages/register.rs
+++ frontend/src/pages/logged_out_pages/register.rs
@@ -1,8 +1,13 @@
 use sauron::prelude::*;
-use crate::messages::{Msg,GoToPage};
+use crate::messages::{Msg,GoToPage,UserRegister};
 use sauron::html::{meta,title,link};
 
 pub fn view() -> Node<Msg> {
+  let mut username : String = String::new();
+  let mut userpassword : String = String::new();
+  let mut userpassword_again : String = String::new();
+  let mut usermail : String = String::new();
+
     node! {
       <main>
     <nav class="navbar navbar-expand navbar-dark bg-dark">
@@ -34,17 +39,80 @@ pub fn view() -> Node<Msg> {
     </div>
     </nav>
     <div class="container-fluid">
-  <div class="row">
-    <div class="col-2">
-      1 of 3
+    <div class="row">
+      <div class="col-2">
+        
+      </div>
+      <div class="col-8" class="col text-start">
+        <h1 class="text-center">"Registration page"</h1>
+        <div class="col-2">
+      </div>
+      </div>
+      <div class="row">
+      <div class="col-5">
+      </div>
+      <div class="col-2">
+        <label for="inputPassword5" class="form-label">"Username"</label>
+    <input
+        type="text"
+        id="inputPassword5"
+        class="form-control"
+        aria-describedby="passwordHelpBlock"
+        value={username.clone()}
+        on_input=|input| Msg::Registration(UserRegister::UpdateUserRegisterName((input.value())))
+    />
+    <div id="passwordHelpBlock" class="form-text">
+        "Enter your username."
     </div>
-    <div class="col-2">
-      2 of 3 (wider)
+
+    <label for="inputPassword5" class="form-label">"Valid email address"</label>
+    <input
+        type="text"
+        id="inputPassword5"
+        class="form-control"
+        aria-describedby="passwordHelpBlock"
+        value={username.clone()}
+        on_input=|input| Msg::Registration(UserRegister::UpdateUserRegisterEmail((input.value())))
+    />
+    <div id="passwordHelpBlock" class="form-text">
+        "Enter your valid email address."
     </div>
-    <div class="col-2">
-      3 of 3
+
+    <label for="inputPassword6" class="form-label">"Password"</label>
+    <input
+        type="password"
+        id="inputPassword6"
+        class="form-control"
+        aria-describedby="passwordHelpBlock"
+        value={userpassword.clone()}
+        on_input=|input| Msg::Registration(UserRegister::UpdateUserRegisterPassword((input.value())))
+    />
+    <div id="passwordHelpBlock" class="form-text">
+    "Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji."
+</div>
+
+    <label for="inputPassword6" class="form-label">"Password checking"</label>
+    <input
+        type="password"
+        id="inputPassword6"
+        class="form-control"
+        aria-describedby="passwordHelpBlock"
+        value={userpassword_again.clone()}
+        on_input=|input| Msg::Registration(UserRegister::UpdateUserRegisterPasswordAgain((input.value())))
+    />
+    <div id="passwordHelpBlock" class="form-text">
+        "Enter chosen password again. This step is necessary in order to prevent typos."
+    </div>
+
+
+    <button class="btn btn-primary w-100" on_click=move |_| Msg::Registration(UserRegister::RegistrationAttempt)>
+        "Register"
+    </button>
+      </div>
+      <div class="col-5">
+      </div>
     </div>
   </div>
-</div>
+  </div>
   </main>
   }}
