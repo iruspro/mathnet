c9ad648832ca784fea367aa8a636feea99f2c454
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-04-28 11:46:44 +0200

    Corrrected some navigation; started creating User and Group structs.

--- frontend/src/user.rs
+++ frontend/src/user.rs
@@ -1,4 +1,5 @@
 
+pub use crate::group_struct::GroupId;
 #[derive(Debug,Clone)]
 pub struct UserLoginData{
     pub user_name : String,
@@ -14,4 +15,16 @@ pub struct UserRegisterData{
 
 pub fn get_user_register_data() -> UserRegisterData {
     UserRegisterData { user_name: String::new(), user_password: String::new(), user_password_again: String::new(), user_email: String::new()}
+}
+
+pub enum UserId{
+UserIdNumber(u32),
+}
+pub struct User{
+    pub user_id : UserId,
+    pub user_name : String,
+    pub user_password : String,
+    pub user_email: String,
+    pub user_friends : Vec<UserId>,
+    pub user_groups: Vec<GroupId>,
 }
