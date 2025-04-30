34823ee8f596f720d0867570d749f7f10653b626
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-04-30 20:17:22 +0200

    feat: create config

--- mathnet-server/src/web/routes_static.rs
+++ mathnet-server/src/web/routes_static.rs
@@ -1,14 +1,13 @@
+use crate::config;
 use axum::handler::HandlerWithoutStateExt;
 use axum::http::StatusCode;
 use axum::routing::{MethodRouter, any_service};
 use tower_http::services::ServeDir;
-
-const WEB_FOLDER: &str = "web-folder";
 
 pub fn serve_dir() -> MethodRouter {
     async fn handle_404() -> (StatusCode, &'static str) {
         (StatusCode::NOT_FOUND, "Resource not found")
     }
 
-    any_service(ServeDir::new(WEB_FOLDER).not_found_service(handle_404.into_service()))
+    any_service(ServeDir::new(&config().WEB_FOLDER).not_found_service(handle_404.into_service()))
 }
