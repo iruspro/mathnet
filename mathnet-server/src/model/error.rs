139b7f44b0eabc977ee90666af2fbfc09d2581a0
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-04-30 23:49:14 +0200

    feat: implement initial model layer structure, first model and unit tests

--- mathnet-server/src/model/error.rs
+++ mathnet-server/src/model/error.rs
@@ -1,9 +1,33 @@
 use serde::Serialize;
+use serde_with::{DisplayFromStr, serde_as};
+
+use super::store;
 
 pub type Result<T> = core::result::Result<T, Error>;
 
+#[serde_as]
 #[derive(Debug, Serialize)]
-pub enum Error {}
+pub enum Error {
+    // -- Modules
+    Store(store::Error),
+
+    // -- Externals
+    Sqlx(#[serde_as(as = "DisplayFromStr")] sqlx::Error),
+}
+
+// region:    --- Froms
+impl From<sqlx::Error> for Error {
+    fn from(val: sqlx::Error) -> Self {
+        Self::Sqlx(val)
+    }
+}
+
+impl From<store::Error> for Error {
+    fn from(val: store::Error) -> Self {
+        Self::Store(val)
+    }
+}
+// endregion: --- Froms
 
 // region:    --- Error Boilerplate
 impl core::fmt::Display for Error {
--- mathnet-server/src/model/mod.rs
+++ mathnet-server/src/model/mod.rs
@@ -1,15 +1,47 @@
+//! Model Later
+//!
+//! Design:
+//!
+//! - The Model layer normalizes the application's data type
+//!   structures and access.
+//! - All application code data access must go through the Model layer.
+//! - The `ModelManager` holds the internal states/resources
+//!   needed by ModelControllers to access data.
+//!   (e.g., db_pool, S3 client, redis client).
+//!  - Model Controllers (e.g., `ThoughtBmc`, `ProjectBmc`) implement
+//!    CRUD and other data access methods on a given "entity"
+//!    (e.g., `Task`, `Project`).
+//!    (`Bmc is shorn for Backend Model Controller`).
+//!  - In frameworks like Axum `ModelManager` are typically used ass App State.
+//!  - ModelManager are designed to be passed as an argument
+//!    to all Model Controllers functions.
+
 // region:    --- Modules
 mod error;
+mod store;
+pub mod thought;
+
+use store::{Db, new_db_pool};
 
 pub use self::error::{Error, Result};
 // endregion: --- Modules
 
 #[derive(Clone)]
-pub struct ModelManager {}
+pub struct ModelManager {
+    db: Db,
+}
 
 impl ModelManager {
+    /// Constructor
     pub async fn new() -> Result<Self> {
-        // FIXME - TBC
-        Ok(ModelManager {})
+        let db = new_db_pool().await?;
+
+        Ok(ModelManager { db })
+    }
+
+    /// Return the sqlx db pool reference.
+    /// (Only for the model layer)
+    pub(in crate::model) fn db(&self) -> &Db {
+        &self.db
     }
 }
