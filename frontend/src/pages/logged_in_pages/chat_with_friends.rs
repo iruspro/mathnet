3be0581ab26833deaa1efe46b15ee91c26963891
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-05-02 14:59:49 +0200

    Started making all neccessary structs for chat channels. Started figuring out how to include friends list at the right sidebar in chat_with_friends.rs

--- frontend/src/pages/logged_in_pages/chat_with_friends.rs
+++ frontend/src/pages/logged_in_pages/chat_with_friends.rs
@@ -1,7 +1,9 @@
 use sauron::prelude::*;
 use crate::messages::{Msg, GoToPage, SwitchToPageSigned};
+use crate::app::App;
+use crate::structs::friends_at_sidebar::{show_friends_at_sidebar};
 
-pub fn view() -> Node<Msg> {
+pub fn view(data_provided : &App) -> Node<Msg> {
     node! {
         <main>
             // Left Sidebar (Desktop)
@@ -27,7 +29,7 @@ pub fn view() -> Node<Msg> {
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToSettings))}>"Settings"</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">"Notifications"</a>
+                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToNotifications))}>"Notifications"</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToLogOut))}>"Log out"</a>
@@ -39,12 +41,11 @@ pub fn view() -> Node<Msg> {
             <div class="right-sidebar d-none d-md-block text-white">
                 <h4>"Right Sidebar"</h4>
                 <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">"Extra 1"</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">"Extra 2"</a>
-                    </li>
+                <div class="search-container">
+        <input type="text" class="form-control search-input" placeholder="Find a person" on_input=|input|{Msg::Search(input.value())}></input>
+        <i class="fas fa-search search-icon"></i>
+      </div>
+                    show_friends_at_sidebar();
                 </ul>
             </div>
 
@@ -53,7 +54,7 @@ pub fn view() -> Node<Msg> {
                 <div class="container-fluid">
                     <h1 class="text-center">"Find friends"</h1>
                     <p class="basicparagraph text-start">
-                        "Groups you belong to.
+                        "View other people's profiles or chat with somebody.
 
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras luctus consectetur placerat. Donec non pretium sapien. Donec ac placerat ex. Aenean tempus massa nulla, nec ullamcorper leo tempus eget. Vestibulum at lectus ut libero ullamcorper consectetur. Vestibulum auctor urna venenatis libero eleifend, in sodales odio dictum. Curabitur auctor, massa eget ultricies efficitur, justo nulla porta purus, in laoreet quam turpis ut risus. Aliquam nunc nibh, placerat eget bibendum varius, porta eget tellus. Phasellus in lacinia augue, sed consequat ex. Curabitur laoreet mi nec eros tristique, ac tristique mauris tempus."
                     </p>
---
+++ frontend/src/structs/emojis_and_reactions.rs
@@ -0,0 +1,5 @@
+pub struct Emoji{
+    pub sort_of_emoji : (),
+    pub number_of_reacts_for_that_emoji : u32,
+
+}
