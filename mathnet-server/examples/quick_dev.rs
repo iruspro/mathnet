3aee347c38b9194e53cb14d09b4bd4d575562511
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-05-04 12:44:14 +0200

    feat: implement basic RPC and Thought entity endpoint

--- mathnet-server/examples/quick_dev.rs
+++ mathnet-server/examples/quick_dev.rs
@@ -7,7 +7,7 @@ use serde_json::json;
 async fn main() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/index.html").await?.print().await?;
+    // hc.do_get("/index.html").await?.print().await?;
 
     let req_login = hc.do_post(
         "/api/login",
@@ -17,6 +17,68 @@ async fn main() -> Result<()> {
         }),
     );
     req_login.await?.print().await?;
+
+    let req_create_thought = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 1,
+            "method": "create_thought",
+            "params": {
+                "data": {
+                    "content": "thought AAAB"
+                }
+            },
+        }),
+    );
+    req_create_thought.await?.print().await?;
+
+    let req_update_thought = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 1,
+            "method": "update_thought",
+            "params": {
+                "id": 1000, // Hardcode the task id.
+                "data": {
+                    "content": "content BB"
+                }
+            },
+        }),
+    );
+    req_update_thought.await?.print().await?;
+
+    let req_get_thought = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 1,
+            "method": "get_thought",
+            "params": {
+                "id": 1000,
+            },
+        }),
+    );
+    req_get_thought.await?.print().await?;
+
+    let req_delete_thought = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 1,
+            "method": "delete_thought",
+            "params": {
+                "id": 1001, // Hardcode the task id
+            }
+        }),
+    );
+    req_delete_thought.await?.print().await?;
+
+    let req_list_thoughts = hc.do_post(
+        "/api/rpc",
+        json!({
+            "id": 1,
+            "method": "list_thoughts"
+        }),
+    );
+    req_list_thoughts.await?.print().await?;
 
     Ok(())
 }
