1c56f025b850970f3b825796b84e1cd5e209d0d0
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-05-10 16:01:53 +0200

    Enabled chat channels display at chat_with_friends.rs with dummy examples; renamed static folder to frontend_design; enabled current local time extraction using chrono crate.

---
+++ frontend/src/logics/date_and_time.rs
@@ -0,0 +1,22 @@
+use chrono::prelude::*;
+
+pub enum DateAndTime{
+    MathNetDate(String)
+}
+pub fn display_current_date_and_time() -> DateAndTime{
+    let time_here_and_now = Local::now();
+    let formated_time_here_and_now = time_here_and_now.format("%F, %X").to_string();
+    DateAndTime::MathNetDate(formated_time_here_and_now)
+}
+
+impl DateAndTime{
+pub fn date_and_time_to_string(&self) -> String{
+    match self {
+        DateAndTime::MathNetDate(time) => time.clone(),
+    }
+}}
+
+/* It should be considered what to do when users travel and switch time zones.
+Current implementation means that if you send a message and then travel abroad and
+change time zone in the process and then send a new message, you will have messages 
+from different time zones, which is weird and confusing. */
