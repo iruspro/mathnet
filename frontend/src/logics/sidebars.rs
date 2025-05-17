d89ed601d35008db697f4beb89afa82e5bf739ca
Benjamin Dagarin <bd94300@student.uni-lj.si>
2025-05-17 10:31:11 +0200

    Managed to write left_sidebar function properly. Now I need to test it.

--- frontend/src/logics/sidebars.rs
+++ frontend/src/logics/sidebars.rs
@@ -1,81 +1,72 @@
 use sauron::prelude::*;
-use crate::messages::{Msg, GoToPage, SwitchToPageSigned};
+use crate::messages::{Msg, GoToPage, SwitchToPageSigned,SwitchToPageOther};
 use crate::app::App;
 use crate::logics::{displaying_friends::{show_friends_at_sidebar,show_chats_in_content}, displaying_conversation};
 use crate::experimental_examples::imaginary_friends;
-use crate::list_of_pages::{Page,SignedPage};
+use crate::list_of_pages::{Page,SignedPage,OtherPage};
 use sauron::node;
 
-fn show_nav_link(page : SignedPage)->Node<Msg>{
+fn show_nav_link(page : Page)->Node<Msg>{
 match page{
-SignedPage::UserProfile => {
+Page::ItemSignedPage(SignedPage::UserProfile) => {
 node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToUserProfile))}>"User profile"</a>
                     </li>
     }
 },
-SignedPage::ChatWithFriends => {
+Page::ItemSignedPage(SignedPage::ChatWithFriends) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToChatWithFriends))}>"Chat with friends"</a>
                     </li>
     }
 }
-SignedPage::GroupsList => {
+Page::ItemSignedPage(SignedPage::GroupsList) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToGroupsList))}>"Groups"</a>
                     </li>
     }
 }
-SignedPage::Docs => {
+Page::ItemSignedPage(SignedPage::Docs) => {
     node!{<li class="nav-item">
-                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToDocs))}>"Docs"</a>
+                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToDocsPage))}>"Docs"</a>
                     </li>
     }
 },
-SignedPage::AboutProject => {
+Page::ItemSignedPage(SignedPage::AboutProject) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToAboutProject))}>"About this project"</a>
                     </li>
     }
 },
-SignedPage::PrivacyAndSecurity => {
+Page::ItemSignedPage(SignedPage::PrivacyAndSecurity) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToPrivacyAndSecurity))}>"Privacy and security"</a>
                     </li>
     }
 },
-SignedPage::Notifications => {
+Page::ItemSignedPage(SignedPage::Notifications) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToNotifications))}>"Notifications"</a>
                     </li>
     }
 },
-SignedPage::Settings => {
+Page::ItemSignedPage(SignedPage::Settings) => {
     node!{<li class="nav-item">
                         <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToSettings))}>"Settings"</a>
                     </li>
     }
 },
-SignedPage::LogOut => {
+Page::ItemSignedPage(SignedPage::LogOut) => {
     node!{<li class="nav-item">
-                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToLogOut))}>"Log out"</a>
+                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageOther(SwitchToPageOther::GoToLogOut))}>"Log out"</a>
                     </li>
     }
 },
-_ => {
-    node!{
-      <li class="nav-item">
-                        <a class="nav-link text-white" on_click=|_|{Msg::SetPage(GoToPage::GoToPageSigned(SwitchToPageSigned::GoToLogOut))}>"Log out"</a>
-                    </li>  
-    }
+_ => unimplemented!()
+}
 }
 
 
-}
-
-    
-
-}
 fn show_active_nav_link(page_name : String)->Node<Msg>{
     node!{
 <li class="nav-item">
@@ -84,10 +75,59 @@ fn show_active_nav_link(page_name : String)->Node<Msg>{
 
 }}
 
+//pub fn left_sidebar(current_page: Page) -> Node<Msg> {
+//    let list_of_signed_pages: [Page; 9] = [
+//        Page::ItemSignedPage(SignedPage::UserProfile),
+//        Page::ItemSignedPage(SignedPage::ChatWithFriends),
+//        Page::ItemSignedPage(SignedPage::GroupsList),
+//        Page::ItemSignedPage(SignedPage::Docs),
+//        Page::ItemSignedPage(SignedPage::AboutProject),
+//        Page::ItemSignedPage(SignedPage::PrivacyAndSecurity),
+//        Page::ItemSignedPage(SignedPage::Notifications),
+//        Page::ItemSignedPage(SignedPage::Settings),
+//        Page::ItemSignedPage(SignedPage::LogOut),
+//    ];
+//
+//    // Generate list items outside the node! macro
+//    let nav_links: Vec<Node<Msg>> = list_of_signed_pages
+//        .iter()
+//        .map(|pagex| {
+//            if *pagex == current_page {
+//                // Extract readable name from current_page for display
+//                let name = match pagex {
+//                    Page::ItemSignedPage(sp) => format!("{:?}", sp), // Or a custom name function
+//                    _ => "Unknown".to_string(),
+//                };
+//                show_active_nav_link(name)
+//            } else {
+//                match pagex {
+//                    Page::ItemSignedPage(sp) => show_nav_link(*sp),
+//                    _ => text!(""), // fallback
+//                }
+//            }
+//        })
+//        .collect();
+//
+//    // Now build the sidebar using the pre-collected nav_links
+//    node! {
+//        <div class="sidebar d-none d-md-block text-white">
+//            <h4>"Sidebar"</h4>
+//            <ul class="nav flex-column">
+//                {nav_links}
+//            </ul>
+//        </div>
+//    }
+//}
+
+
+
+
 
 /* Handling left sidebar display*/
-pub fn left_sidebar(current_page : Page)-> Node<Msg>{
-    let list_of_signed_pages :[Page;9]=[
+
+/*
+pub fn left_sidebar(current_page : Page) -> Node<Msg>{
+let list_of_signed_pages :[Page;9]=[
 Page::ItemSignedPage(SignedPage::UserProfile),
 Page::ItemSignedPage(SignedPage::ChatWithFriends), 
 Page::ItemSignedPage(SignedPage::GroupsList),
@@ -98,8 +138,19 @@ Page::ItemSignedPage(SignedPage::Notifications),
 Page::ItemSignedPage(SignedPage::Settings),
 Page::ItemSignedPage(SignedPage::LogOut),
     ];
-
+node!{
+for pagex in list_of_signed_pages{
+if pagex == current_page{
+    node!{
+            <div class="sidebar d-none d-md-block text-white">
+                <h4>"Sidebar"</h4>
+                <ul class="nav flex-column">
+                    show_active_nav_link(current_page)
+                </ul>
+            </div>
 }
+}
+else {
 node!{
             <div class="sidebar d-none d-md-block text-white">
                 <h4>"Sidebar"</h4>
@@ -107,21 +158,63 @@ node!{
                     show_nav_link(current_page)
                 </ul>
             </div>
-}
+}}}}}
+*/
 
 
 /* Handling left sidebar display*/
-pub fn right_sidebar() -> Node<Msg> {
-node!{
-    <div class="right-sidebar d-none d-md-block text-white">
-                <h4>"Right Sidebar"</h4>
-                <ul class="nav flex-column">
-                <div class="search-container">
-        <input type="text" class="form-control search-input" placeholder="Find a person" on_input=|input|{Msg::SearchFriend(input.value())}></input>
-        <i class="fas fa-search search-icon"></i>
-      </div>
-                    //{show_friends_at_sidebar()};
-                </ul>
-            </div>
+//pub fn right_sidebar() -> Node<Msg> {
+//node!{
+//    <div class="right-sidebar d-none d-md-block text-white">
+//                <h4>"Right Sidebar"</h4>
+//                <ul class="nav flex-column">
+//                <div class="search-container">
+//        <input type="text" class="form-control search-input" placeholder="Find a person" on_input=|input|{Msg::SearchFriend(input.value())}></input>
+//        <i class="fas fa-search search-icon"></i>
+//      </div>
+//                    //{show_friends_at_sidebar()};
+//                </ul>
+//            </div>
+//
+//}}}
 
-}}
+use sauron::prelude::*;
+
+// Assume Msg and Page/SignedPage types are already defined
+
+pub fn left_sidebar(current_page: Page) -> Node<Msg> {
+    let list_of_signed_pages: [Page; 9] = [
+        Page::ItemSignedPage(SignedPage::UserProfile),
+        Page::ItemSignedPage(SignedPage::ChatWithFriends),
+        Page::ItemSignedPage(SignedPage::GroupsList),
+        Page::ItemSignedPage(SignedPage::Docs),
+        Page::ItemSignedPage(SignedPage::AboutProject),
+        Page::ItemSignedPage(SignedPage::PrivacyAndSecurity),
+        Page::ItemSignedPage(SignedPage::Notifications),
+        Page::ItemSignedPage(SignedPage::Settings),
+        Page::ItemOtherPage(OtherPage::LogOut),
+    ];
+
+    // Generate nav links as a Vec<Node<Msg>>
+    let nav_links: Vec<Node<Msg>> = list_of_signed_pages
+        .iter()
+        .map(|pagex| {
+            if *pagex == current_page {
+                show_active_nav_link(Page::page_name_to_string(pagex.clone()))
+            } else {
+                show_nav_link(pagex.clone())
+            }
+        })
+        .collect();
+
+    node! {
+        <div class="sidebar d-none d-md-block text-white">
+            <h4>"Sidebar"</h4>
+            <ul class="nav flex-column">
+                node!{for el in nav_links{
+                    el
+                }}
+            </ul>
+        </div>
+    }
+}
