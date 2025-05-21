aada6daabd94e155121c94c820f6799b22f4da04
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-05-21 22:13:30 +0200

    chore: update pre-commit.sh

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,15 +3,15 @@
 set -e
 
 echo "Running cargo fmt (mathnet-server)..."
-cargo fmt --manifest-path mathnet-server/Cargo.toml --all -- --check
+cargo fmt --manifest-path mathnet-server/Cargo.toml --all --check
 
 echo "Running cargo clippy (mathnet-server)..."
 cargo clippy --manifest-path mathnet-server/Cargo.toml --all-targets --all-features -- -D warnings
 
-# echo "Running cargo fmt (mathnet-client)..."
-# cargo fmt --manifest-path client/Cargo.toml --all -- --check
+echo "Running cargo fmt (mathnet-client)..."
+cargo fmt --manifest-path frontend/Cargo.toml --all --check
 
 # echo "Running cargo clippy (mathnet-client)..."
-# cargo clippy --manifest-path client/Cargo.toml --all-targets --all-features -- -D warnings
+# cargo clippy --manifest-path frontend/Cargo.toml --all-targets --all-features -- -D warnings
 
 echo "All checks passed."
