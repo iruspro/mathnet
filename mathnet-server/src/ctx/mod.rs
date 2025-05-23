3432be93b371393e3efdc05f06b9cfbfb85b5fb5
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-05-23 09:45:04 +0200

    feat: merge mather and user

--- mathnet-server/src/ctx/mod.rs
+++ mathnet-server/src/ctx/mod.rs
@@ -6,27 +6,27 @@ pub use self::error::{Error, Result};
 
 #[derive(Clone, Debug)]
 pub struct Ctx {
-    user_id: i64,
+    mather_id: i64,
 }
 
 // Constructor.
 impl Ctx {
     pub fn root_ctx() -> Self {
-        Ctx { user_id: 0 }
+        Ctx { mather_id: 0 }
     }
 
-    pub fn new(user_id: i64) -> Result<Self> {
-        if user_id == 0 {
+    pub fn new(mather_id: i64) -> Result<Self> {
+        if mather_id == 0 {
             Err(Error::CtxCannotNewRootCtx)
         } else {
-            Ok(Self { user_id })
+            Ok(Self { mather_id })
         }
     }
 }
 
 // Property Accessors.
 impl Ctx {
-    pub fn user_id(&self) -> i64 {
-        self.user_id
+    pub fn mather_id(&self) -> i64 {
+        self.mather_id
     }
 }
