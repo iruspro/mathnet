221e28ff61f3a8fbdc87e317d59e009b1cb87d55
Ruslan Urazbakhtin <ruspro.business@gmail.com>
2025-05-31 17:09:08 +0200

    chore: update .gitignore, rust.yml, DEVELOPMENT.md and pre-commit.sh files

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -8,10 +8,10 @@ cargo fmt --manifest-path mathnet-server/Cargo.toml --all --check
 echo "Running cargo clippy (mathnet-server)..."
 cargo clippy --manifest-path mathnet-server/Cargo.toml --all-targets --all-features -- -D warnings
 
-echo "Running cargo fmt (mathnet-client)..."
-cargo fmt --manifest-path frontend/Cargo.toml --all --check
+echo "Running cargo fmt (mathnet-frontend)..."
+cargo fmt --manifest-path mathnet-frontend/Cargo.toml --all --check
 
-# echo "Running cargo clippy (mathnet-client)..."
-# cargo clippy --manifest-path frontend/Cargo.toml --all-targets --all-features -- -D warnings
+echo "Running cargo clippy (mathnet-frontend)..."
+cargo clippy --manifest-path mathnet-frontend/Cargo.toml --all-targets --all-features -- -D warnings
 
 echo "All checks passed."
